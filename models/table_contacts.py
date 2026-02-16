c04a29c5a47ece364aa60753b6d0cc2a829af20f
marianob <marianoborgini1@gmail.com>
2026-02-15 23:25:44 -0300

    update.html centrado - required email quitado

--- models/table_contacts.py
+++ models/table_contacts.py
@@ -6,5 +6,5 @@ class Contactos(db.Model):
     __tablename__ = 'contactos'
     id = db.Column(db.Integer, primary_key = True)
     nombre = db.Column(db.String(50), nullable=False)
-    telefono = db.Column(db.Integer)
+    telefono = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(100))
